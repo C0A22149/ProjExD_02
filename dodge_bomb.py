71f7cdb97fc4032fc9d20c6f655d9ec7c27403d9
yoshida <c0a22149bf@edu.teu.ac.jp>
2023-11-28 14:08:41 +0900

    爆弾生成

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,5 +1,6 @@
 import sys
 import pygame as pg
+import random
 
 
 WIDTH, HEIGHT = 1600, 900
@@ -12,6 +13,12 @@ def main():
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
     clock = pg.time.Clock()
+    bomb_place = [random.randint(0,WIDTH),random.randint(0,HEIGHT)]
+    # bomb_place = (10,10)
+    bomb_harf = 10
+    bomb = pg.Surface((bomb_harf*2,bomb_harf*2))
+    pg.draw.circle(bomb,(255,0,0),(bomb_harf,bomb_harf),bomb_harf)
+    bomb.set_colorkey((0,0,0))
     tmr = 0
     while True:
         for event in pg.event.get():
@@ -20,6 +27,7 @@ def main():
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        screen.blit(bomb,bomb_place)
         pg.display.update()
         tmr += 1
         clock.tick(10)
